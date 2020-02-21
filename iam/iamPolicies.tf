b90572cc911b54b4f8656b45795b3661bce65d4b
George Werbacher <grwerbacher@gmail.com>
2020-02-20 21:09:05 -0500

    testing

--- iam/iamPolicies.tf
+++ iam/iamPolicies.tf
@@ -15,9 +15,3 @@ resource "aws_iam_policy" "s3Admin3" {
   path = "/"
   policy = "${file("./iam/policies/s3Admin.json")}"
 }
-
-resource "aws_iam_policy" "ec2-Admin" {
-  name = "ec2-admin-policy"
-  path = "/"
-  policy = "${file("./iam/policies/ec2Admin.json")}"
-}
