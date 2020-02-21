312fd8480339bbc0a8faea714e88e26bc6c06475
George Werbacher <grwerbacher@gmail.com>
2020-02-21 08:38:59 -0500

    test

--- iam/iamPolicies.tf
+++ iam/iamPolicies.tf
@@ -10,26 +10,8 @@ resource "aws_iam_policy" "s3Admin" {
   policy = "${file("./iam/policies/s3Admin.json")}"
 }
 
-resource "aws_iam_policy" "ec2Admin2" {
-  name = "ec2-admin-policy2"
-  path = "/"
-  policy = "${file("./iam/policies/ec2Admin.json")}"
-}
-
-resource "aws_iam_policy" "ec2Admin3" {
-  name = "ec2-admin-policy3"
-  path = "/"
-  policy = "${file("./iam/policies/ec2Admin.json")}"
-}
-
-resource "aws_iam_policy" "ec2Admin4" {
-  name = "ec2-admin-policy4"
-  path = "/"
-  policy = "${file("./iam/policies/ec2Admin.json")}"
-}
-
-resource "aws_iam_policy" "ec2Admin5" {
-  name = "ec2-admin-policy5"
+resource "aws_iam_policy" "ec2Admin" {
+  name = "ec2-admin-policy"
   path = "/"
   policy = "${file("./iam/policies/ec2Admin.json")}"
 }
