2045a809b29089e978a72cc4065dc2f6f0db2d24
George Werbacher <grwerbacher@gmail.com>
2020-02-23 15:44:30 -0500

    testing

--- iam/iamAttach.tf
+++ iam/iamAttach.tf
@@ -2,7 +2,7 @@
 ## Attached Policy: ec2-admin-policy
 ## Intake: xxxxxx
 ## Created By: George Werbacher
-resource "aws_iam_role_policy_attachment" "ec2Admin" {
+resource "aws_iam_role_policy_attachment" "ec2-Admin" {
   role = "${aws_iam_role.role.name}"
   policy_arn = "${aws_iam_policy.ec2Admin.arn}"
 }
